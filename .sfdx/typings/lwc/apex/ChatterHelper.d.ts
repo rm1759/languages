1c1b1c0fd05502cc8e08b9f33390f46e919c0b35
Rob Miller <rm1759@gmail.com>
2020-02-27 15:36:18 -0500

    test

---
+++ .sfdx/typings/lwc/apex/ChatterHelper.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/ChatterHelper.getChatterGroups" {
+  export default function getChatterGroups(param: {imageUrl: any, chatterText: any}): Promise<any>;
+}
---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/resourceUrl/GIPHY" {
+    var GIPHY: string;
+    export default GIPHY;
+}
+declare module "@salesforce/resourceUrl/jquery331" {
+    var jquery331: string;
+    export default jquery331;
+}
